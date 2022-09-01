9543218c1ac211138f2be6788a4ae92bbc131ead
AlexKor94 <sasha1994work@gmail.com>
2022-09-01 08:45:36 +0300

    download Jeka script, started obj

--- testScripts.js
+++ testScripts.js
@@ -1,10 +1,27 @@
+"use strict";
 // callBack
 function withCallBack(src,callBack){
   console.log("src");
+  src = " - link for second function";
   let someNewValue = ()=> callBack(src);
+  someNewValue();
 }
 
-withCallBack("some link",()=>{
-  console.log("second value");
-
+withCallBack("some link",(link)=>{
+  console.log(`second value ${link}`);
 });
+
+// object loop
+function workWithOptionObj(){
+  const options = {
+    name: "area",
+    width: 100,
+    height: 30,
+    colors: {
+      border: "black",
+      background: "red"
+    }
+  };
+  console.log(options.name);
+}
+workWithOptionObj();
