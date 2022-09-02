6f17f2e6480c0588d4691754a0a27ec1b0f7cff8
AlexKor94 <sasha1994work@gmail.com>
2022-09-02 07:25:57 +0300

    copy obj, array

--- testScripts.js
+++ testScripts.js
@@ -48,3 +48,109 @@ const {border, bg} = options.colors;
 console.log(border);
 }
 workWithOptionObj();
+
+// Array
+function testArr(){
+  const arr =[1,2,3,6,8];
+ arr.pop();
+ arr.push(10);
+  console.log(arr);
+
+  for (let vel of arr){
+    console.log(vel);
+  }
+
+  arr[99] = 0;
+  console.log(arr.length);
+  console.log(arr);
+
+  arr.forEach((item, i, arrLink) => {
+    console.log(`${i}: ${item} in array: ${arrLink}`);
+  });
+
+  const str = "computer, leptop, monitor";
+  const products = str.split(", ");
+  console.log(products);
+
+  let newString = products.join(', ');
+
+  console.log(newString);
+
+  let numbersArr = [2,13,26,8,10];
+  numbersArr.sort();
+  console.log(numbersArr);
+  numbersArr.sort(compareNum);
+  console.log(numbersArr);
+}
+
+function compareNum(a, b) {
+  return a - b;
+}
+testArr();
+
+// Переда даних по посиланню і по значенню
+// Data transfer by link and value
+
+function transferData(){
+  let a = 5,
+  b = a;
+
+  b = b + 5;
+  console.log(b);
+  console.log(a);
+
+  const obj = {
+    a: 5,
+    b: 1
+  }
+  const copy = obj;
+  copy["a"] = 10;
+  console.log(copy);
+  console.log(obj);
+
+  let newObj = copyObj(obj);
+  newObj["a"] = 11;
+  console.log(newObj);
+  console.log(obj);
+
+  const addNewObj = {
+    d: 17,
+    e: 20
+  }
+  
+  const clone = Object.assign({},addNewObj);
+  clone["d"] = 1;
+  console.log(clone);
+  console.log(addNewObj);
+}
+
+function copyObj(obj){
+  let objCopy = {};
+
+  let key;
+  for(key in obj){
+    objCopy[key] = obj[key];
+  }
+
+  return objCopy;
+}
+
+// cpy arr
+
+function copyArr(){
+  const oldArray = ['a','b','c'];
+  const newArray = oldArray.slice();
+  newArray[2] = "d";
+  console.log(oldArray);
+  console.log(newArray);
+}
+
+//оператор разворота
+// deployment operator
+function deploymentArray(){
+  const video = ["youtube","udemy"],
+        blogs = ['blogger', 'facebook', 'instagram'],
+        internet = [...video, ...blogs, 'Gmail'];
+  console.log(internet);
+}
+deploymentArray();
